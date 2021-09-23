a579e623ef82c6ed80113a0314dc203aa51d828c
kailas <kailas.shekar@gmail.com>
2021-09-22 23:45:25 -0700

    Binance accounts refresh logic

---
+++ refreshBinanceAccounts.py
@@ -0,0 +1,48 @@
+import json
+import requests
+import boto3
+
+dynamoClient = boto3.client("dynamodb")
+
+def getAttribute(dbItem, attributeName, attributeType):
+    """
+    INPUT
+    dbItem: the databse item returned => {'headers' : {'S': 'some string'}, ...}
+    attribtueName: name of the item
+    attributeType: one of the dynamoDb datatypes => {'S', 'N'...}
+
+    OUTPUT
+    return database attribute 
+    """
+    return dbItem.get(attributeName, {}).get(attributeType, "")
+
+def lambda_handler(event, context):
+    # TODO implement
+    expression_attribute_names = {
+        '#UID' : 'userId',
+        "#AK" : 'api_key'
+    }
+    projection_expression = "#UID, #AK"
+    binance_accounts = dynamoClient.scan(
+        ExpressionAttributeNames=expression_attribute_names,
+        ProjectionExpression=projection_expression,
+        TableName='binanceAccount'
+    ).get("Items",[])
+
+    try:
+        for account in binance_accounts:
+            api_key = getAttribute(account, 'api_key', 'S')
+            userId = getAttribute(account, 'userId', 'S')
+            print("Refreshing for user: {}".format(userId))
+            refresh_url = "https://ji1g9w5p36.execute-api.us-west-1.amazonaws.com/test/binance/refreshdata?api_key={}".format(api_key)
+            requests.request("GET", url=refresh_url)
+    except Exception as e:
+        print("Found error: {}".format(e))
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Error: {}".format(e))
+        }
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
